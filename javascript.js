737e3b054a8eef306659cb00a044c6c094f5f0ae
demandsupply <giova.zol92@gmail.com>
2025-05-04 14:18:40 +0200

    Feat: add input to accept value from 1 to 100 which is equal to the number of pixels per side. Implemented function to dynamically populate the grid with the desired number of pixels

--- javascript.js
+++ javascript.js
@@ -1,16 +1,42 @@
 const grid = document.querySelector(".grid-container");
-
+const btn = document.querySelector(".btn");
 const pixelsPerSide = 16;
-let pixels = pixelsPerSide * pixelsPerSide;
-let pixelSize = grid.clientWidth / pixelsPerSide;
-console.log("grid size is " + pixelSize);
+console.log("start value: " + pixelsPerSide)
 
-for (let i = 0; i < pixels; i++) {
-    let pixel = document.createElement("div");
-    pixel.style.width = `${pixelSize}px`;
-    pixel.style.height = `${pixelSize}px`;
-    pixel.style.backgroundColor = "yellow";
-    pixel.innerText = i;
-    pixel.classList.add("pixel");
-    grid.appendChild(pixel);
+
+btn.addEventListener("click", () => {
+    const pixelsPerSide = parseInt(document.getElementById("size").value);
+    console.log(pixelsPerSide)
+    if(isNaN(pixelsPerSide)) {
+        alert("insert a number");
+    }
+    if (pixelsPerSide < 1 || pixelsPerSide > 100) {
+        alert("insert a number between 1 and 100");
+    }
+    console.log("final value: " + pixelsPerSide)
+    clearGrid(grid);
+    createGrid(pixelsPerSide);
+
+})
+
+function createGrid(pixelsPerSide) {
+    let pixels = pixelsPerSide * pixelsPerSide;
+    let pixelSize = grid.clientWidth / pixelsPerSide;
+    console.log("grid size is " + pixelSize);
+    
+    for (let i = 0; i < pixels; i++) {
+        let pixel = document.createElement("div");
+        pixel.style.width = `${pixelSize}px`;
+        pixel.style.height = `${pixelSize}px`;
+        pixel.style.backgroundColor = "yellow";
+        pixel.innerText = i + 1;
+        pixel.classList.add("pixel");
+        grid.appendChild(pixel);
+    }
 }
+
+function clearGrid(grid) {
+    grid.innerText = "";
+}
+
+createGrid(pixelsPerSide);
